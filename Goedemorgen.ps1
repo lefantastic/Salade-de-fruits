e259316a8a51a5c0e96e84d3b7114e54b7ced873
lefantastic <fantastic.seattle@outlook.com>
2017-07-17 15:53:32 -0700

    Updated Goedemorgen.ps1 in Azure Automation.

--- Goedemorgen.ps1
+++ Goedemorgen.ps1
@@ -1,9 +1,14 @@
 <#
 .SYNOPSIS
     Provides a simple example of a Azure Automation runbook.  
+    Edited in Azure
 
 .DESCRIPTION
-    This runbook provides the "Hello World" example for Azure Automation....   
+    This runbook provides the "Hello World" example for Azure Automation....
+
+    The runbook takes in an optional string parameter.  If you leave the parameter blank, the 
+    default of $Name will equal "World".  The runbook then prints "Hello" concatenated with $Name.
+   
 
 .PARAMETER Name
     String value to print as output
