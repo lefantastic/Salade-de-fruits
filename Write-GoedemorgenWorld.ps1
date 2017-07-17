8eef8d26051e95c80641ceed669eb5135d0c6cb9
lefantastic <fantastic.seattle@outlook.com>
2017-07-17 16:02:58 -0700

    Created Write-GoedemorgenWorld.ps1 in AzureAutomation.

---
+++ Write-GoedemorgenWorld.ps1
@@ -0,0 +1,33 @@
+<#
+.SYNOPSIS
+    Provides a simple example of a Azure Automation runbook.  
+    Edited in Azure x2
+
+.DESCRIPTION
+    This runbook provides the "Hello World" example for Azure Automation....
+
+    description deleted from Github/web.
+   
+
+.PARAMETER Name
+    String value to print as output
+
+.EXAMPLE
+    Write-HelloWorld -Name "World"
+
+.NOTES
+	Author: System Center Automation Team 
+	Last Updated: 3/3/2014   
+#>
+
+workflow Write-GoedemorgenWorld {
+    param (
+        # Optional parameter of type string. 
+        # If you do not enter anything, the default value of Name 
+        # will be World
+        [parameter(Mandatory=$false)]
+        [String]$Name = "World"
+    )
+
+        Write-Output "Goedemorgen $Name"
+}
